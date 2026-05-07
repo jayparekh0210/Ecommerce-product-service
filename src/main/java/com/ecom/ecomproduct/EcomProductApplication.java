a7390e24bc23b29b187a30a7a57f443b154b85fb
Jay <jayparekhin02@gmail.com>
2026-05-07 15:30:30 +0530

    Added config server

--- src/main/java/com/ecom/ecomproduct/EcomProductApplication.java
+++ src/main/java/com/ecom/ecomproduct/EcomProductApplication.java
@@ -3,11 +3,15 @@ package com.ecom.ecomproduct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class EcomProductApplication {
 
     static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
         SpringApplication.run(EcomProductApplication.class, args);
     }
+
 
 }
